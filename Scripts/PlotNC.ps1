8f20f4eb96d1f085fd9bc5fcfd21efc5c715daf4
Rock411 <yangln@126.com>
2026-02-16 15:15:05 +0100

    Update PlotNC.ps1

--- Scripts/PlotNC.ps1
+++ Scripts/PlotNC.ps1
@@ -1,19 +1,19 @@
-# ===============================
-# GMT plot script (PowerShell + GMT 6)
+# =================================================
+# Topography map from XYZ data (PowerShell + GMT 6)
 # Example Reigion: Tyrrhenian Sea - UTM 33N
-# Data:
-# ===============================
+# Data: Network Common Data Format (NetCDF)
+# =================================================
 
-# 1. 输入数据
-$grd = "C:\Users\yangln\Desktop\Postdoc\CNR_Italy\Maps\GEBCO\GEBCO_17_Feb_2025\gebco_2024_n44.0_s37.0_w8.0_e17.0.nc"
+# 1. Read Data (.nc is a ready grid file)
+$grd = "gebco_2024_n44.0_s37.0_w8.0_e17.0.nc"
 
-# 2. 研究区域（lon/lat）
+# 2. Example Region（lon/lat）
 $R = "-R8/17/37/44"
 
-# 3. 投影（WGS84 / UTM zone 33N）
+# 3. Projection（WGS84 / UTM zone 33N）
 $J = "-JU33N/15c"
 
-# 4. 计算最大绝对高程（用于对称色标）
+# 4. Calculate the maximum elevation（For symmetric colorbar）
 $range = gmt grdinfo $grd $R -T
 $range = $range -replace "^-T", ""
 
@@ -23,22 +23,22 @@ $zmax  = [double]$parts[1]
 $max   = [Math]::Max([Math]::Abs($zmin), [Math]::Abs($zmax))
 
 
-# 5. 开始 GMT 绘图（输出 PNG，也可改成 ps/pdf）
+# 5. GMT Plot（output format is png, ps, pdf, tiff, note geotiff does not work）
 gmt begin Tyrrhenian_Sea_UTM33 png
 
-    # 6. 生成 CPT（蓝海 + 绿陆，类似 MATLAB）
+    # 6. generate Palette（Blue Sea + Green Land）
     gmt makecpt -Cgeo "-T-$($max)/$($max)/10" -Z
 
-    # 7. 绘制地形
+    # 7. Plot mophorology
     gmt grdimage $grd $R $J -C -Baf -BWSen
 
-    # 8. 海岸线（增强地理可读性）
+    # 8. Plot Caostline
     gmt coast $R $J "-W0.5p" -Df
 
-    # 9. 色标（下方居中）
+    # 9. Colour bar（Bottom Midlle）
     gmt colorbar "-DJBC+w12c/0.5c+o0c/1c" -Bxa -By+l"Elevation / Depth (m)"
 
-    # 10. 标题
+    # 10. Add Title
     "8.5 43.5 Tyrrhenian Sea (WGS84 / UTM 33N)" |
         gmt text -R -J "-F+f14p,Helvetica-Bold+jTC"
 
